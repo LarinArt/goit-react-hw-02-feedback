250fb65352e755a307569ce93a5557b7a3ec7b51
Larin Artem <larin_art@ukr.net>
2022-07-01 23:10:01 +0300

    HW 1 commit

---
+++ src/components/ui/StatisticsItem.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Statistics = styled.li`
+  margin-bottom: 15px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
---
+++ src/styleConfig/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    mainTextColor: '#000000',
+    secondTextColor: '#ffffff',
+    accentTextColor: '#008000',
+
+    mainBgColor: '#ffffff',
+    secondBgColor: '#000000',
+    accentBgColor: '#008000',
+
+    notificationMessage: '#cccccc',
+    notificationMessageGreen: 'rgba(0, 128, 0, 0.2)',
+    notificationMessageRed: 'rgba(255, 0, 0, 0.2)',
+
+    mainAnimationHover: '250ms linear',
+    mainShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.5)',
+  },
+};
